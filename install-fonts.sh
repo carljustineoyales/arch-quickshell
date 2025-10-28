bfb1ee2100ea8390eb5ff099b8ebf2937a7e82c6
carljustineoyales <carljustineoyales@gmail.com>
2025-10-29 01:13:59 +0800

    refactor(shell_script): Refactor fonts installation script

--- install-fonts.sh
+++ install-fonts.sh
@@ -8,20 +8,17 @@ installFonts() {
   mkdir -p "$FONT_DIR"
 
   # Outlined
-  curl -L -o "$FONT_DIR/MaterialSymbolsOutlined.ttf" \
-  https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.ttf
+  curl -L -o "$FONT_DIR/MaterialSymbolsOutlined.ttf" https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsOutlined%5BFILL,GRAD,opsz,wght%5D.ttf
 
   # Rounded
-  curl -L -o "$FONT_DIR/MaterialSymbolsRounded.ttf" \
-  https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsRounded%5BFILL,GRAD,opsz,wght%5D.ttf
+  curl -L -o "$FONT_DIR/MaterialSymbolsRounded.ttf" https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsRounded%5BFILL,GRAD,opsz,wght%5D.ttf
 
   # Sharp
-  curl -L -o "$FONT_DIR/MaterialSymbolsSharp.ttf" \
-  https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsSharp%5BFILL,GRAD,opsz,wght%5D.ttf
+  curl -L -o "$FONT_DIR/MaterialSymbolsSharp.ttf" https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsSharp%5BFILL,GRAD,opsz,wght%5D.ttf
 
   # Refresh cache
   echo "Refreshing font cache"
-  
+
   fc-cache -fv > /dev/null
 
   echo "Done installing fonts"
